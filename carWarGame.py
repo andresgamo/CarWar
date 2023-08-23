572ca2e4e9d3ca556fe083a569c62ab85459d16e
andresgamo <andresgarciamolina3@gmail.com>
2023-08-23 16:28:52 -0500

    Created shufflle method inside Deck class. Corrected redundancies in the generateDeck method at Deck class.

--- carWarGame.py
+++ carWarGame.py
@@ -1,3 +1,5 @@
+import random
+
 class Card:
 
     values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K':13, 'A':14}
@@ -18,13 +20,13 @@ class Deck:
     suits = ['♣','♦','♥','♠']
 
     def __init__(self):
-       self.cards = self.generateDeck(Deck.ranks,Deck.suits)
+       self.cards = self.generateDeck()
 
 
-    def generateDeck(self,ranks, suits):
+    def generateDeck(self) -> list:
         deck = []
-        for suit in suits:
-            for rank in ranks:
+        for suit in self.suits:
+            for rank in self.ranks:
                 card = Card(rank,suit)
                 deck.append(card)
         return deck
@@ -33,8 +35,11 @@ class Deck:
         for card in self.cards:
             print(card)
     
+    def shuffle(self):
+        random.shuffle(self.cards)
+    
     def __str__(self) -> str:
-        return f'{len(self.deck)}'
+        return f'{len(self.cards)}'
     
 
 if __name__ == '__main__':
