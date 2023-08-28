e9b503d32141d16cc37fb68e57c36601b9f178a6
andresgamo <andresgarciamolina3@gmail.com>
2023-08-28 16:44:23 -0500

    Changed from print to logger.

--- carWarGame.py
+++ carWarGame.py
@@ -1,74 +1,84 @@
 import random
+import logging
+
+logging.basicConfig(level=logging.NOTSET)
+logger = logging.getLogger(__file__)
+
 
 class Card:
-
-    values = {'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':11,'Q':12,'K':13,'A':14}
+    values = {
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "10": 10,
+        "J": 11,
+        "Q": 12,
+        "K": 13,
+        "A": 14,
+    }
 
     def __init__(self, rank, suit):
-
         self.rank = rank
         self.suit = suit
         self.value = Card.values[self.rank]
 
     def __str__(self) -> str:
-        return f'{self.rank}{self.suit}'
+        return f"{self.rank}{self.suit}"
+
 
 class Deck:
-
-    ranks = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-    suits = ['♣','♦','♥','♠']
+    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    suits = ["♣", "♦", "♥", "♠"]
 
     def __init__(self) -> None:
         self.cards = self.generate_deck()
 
     def generate_deck(self) -> list:
-        '''Generates a full deck of cards.'''
+        """Generates a full deck of cards."""
         return [Card(rank, suit) for suit in self.suits for rank in self.ranks]
 
     def show_deck(self) -> None:
-        '''Shows in console the cards that are on the pit.'''
+        """Shows in console the cards that are on the pit."""
         for card in self.cards:
             print(card)
 
     def shuffle(self) -> None:
-        '''Shuffles the deck of cards'''
+        """Shuffles the deck of cards"""
         random.shuffle(self.cards)
 
     def __str__(self) -> str:
-        return f'{len(self.cards)}'
+        return f"{len(self.cards)}"
+
 
 class Player:
-
     def __init__(self, name, cards):
         self.name = name
         self.cards = cards
 
     def show_deck(self) -> None:
-        '''Displays all player's hand in console.'''
+        """Displays all player's hand in console."""
         for card in self.cards:
-            print(card)
+            logger.info(card)
 
     def bet(self) -> Card:
-        '''Remove the top card from player's hand and return it'''
+        """Remove the top card from player's hand and return it"""
         return self.cards.pop(0)
 
-    def add_cards (self, cards_pit:list) -> None:
-        '''Add cards from pit to the bottom of the player's hand'''
+    def add_cards(self, cards_pit: list) -> None:
+        """Add cards from pit to the bottom of the player's hand"""
         for card in cards_pit:
             self.cards.append(card)
 
     def __str__(self) -> str:
-        return f'Name: {self.name}\nCards: {self.cards} '
+        return f"Name: {self.name}\nCards: {self.cards} "
 
-if __name__ == '__main__':
-    # deck = Deck()
-    # deck.shuffle()
-    # player1 = Player('player1',deck.cards[:int(len(deck.cards)/2)])
-    # player2 = Player('player2',deck.cards[int(len(deck.cards)/2):])
-    # player1.showDeck()
-    # print(len(player1.cards))
-    # print('\n')
-    # player1.addCards([player1.bet()])
-    # player1.showDeck()
-    # print(len(player1.cards))
-    pass
+
+if __name__ == "__main__":
+    deck = Deck()
+    player1 = Player("player1", deck.cards[: int(len(deck.cards) / 2)])
+    player1.show_deck()
