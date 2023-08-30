889c384bf041394d3962e8dc72e61c98ebd38c01
andresgamo <103901175+andresgamo@users.noreply.github.com>
2023-08-30 10:21:46 -0500

    Deck class (#5)
    * agregar espacio
    
    * Created shufflle method inside Deck class.
    Corrected redundancies in the generateDeck method at Deck class.
    
    * fixed bug in generateDeck method
    
    * Update carWarGame.py
    
    Co-authored-by: Juan Jose Palacio Uribe <40274999+jujopalacio@users.noreply.github.com>
    
    * Added docstring and type-hint
    
    * Removed withspaces and change  show_deck method's name to snake_case.
    
    * optimized generate_deck method
    
    * Added playerClass to deckClass branch
    
    * Added doctring, type-hints and
     changed names to snake case.
    
    * Changed from print to logger
    
    * Removed withspaces and change  show_deck method's name to snake_case.
    
    * Changed from print to logger.
    
    * Changed __str__ from Player Class for a better user experience.
    
    * Changed from print to logging
    
    ---------
    
    Co-authored-by: Juan Jose Palacio Uribe <40274999+jujopalacio@users.noreply.github.com>

--- carWarGame.py
+++ carWarGame.py
@@ -1,42 +1,84 @@
-class Card:
+import random
+import logging
 
-    values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K':13, 'A':14}
+logging.basicConfig(level=logging.NOTSET)
+logger = logging.getLogger(__file__)
+
+
+class Card:
+    values = {
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "10": 10,
+        "J": 11,
+        "Q": 12,
+        "K": 13,
+        "A": 14,
+    }
 
     def __init__(self, rank, suit):
-
         self.rank = rank
         self.suit = suit
         self.value = Card.values[self.rank]
 
     def __str__(self) -> str:
-        return f'{self.rank}{self.suit}{self.value}'
+        return f"{self.rank}{self.suit}"
 
 
 class Deck:
+    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    suits = ["♣", "♦", "♥", "♠"]
 
-    ranks =  ['2', '3', '4', '5', '6', '7', '8', '9','10', 'J', 'Q', 'K', 'A']
-    suits = ['♣','♦','♥','♠']
+    def __init__(self) -> None:
+        self.cards = self.generate_deck()
 
-    def __init__(self):
-       self.cards = self.generateDeck(Deck.ranks,Deck.suits)
+    def generate_deck(self) -> list:
+        """Generates a full deck of cards."""
+        return [Card(rank, suit) for suit in self.suits for rank in self.ranks]
 
-
-    def generateDeck(self,ranks, suits):
-        deck = []
-        for suit in suits:
-            for rank in ranks:
-                card = Card(rank,suit)
-                deck.append(card)
-        return deck
-    
-    def showDeck (self):
+    def show_deck(self) -> None:
+        """Shows in console the cards that are on the pit."""
         for card in self.cards:
-            print(card)
-    
-    def __str__(self) -> str:
-        return f'{len(self.deck)}'
-    
+            logger.info(card)
 
-if __name__ == '__main__':
+    def shuffle(self) -> None:
+        """Shuffles the deck of cards"""
+        random.shuffle(self.cards)
+
+    def __str__(self) -> str:
+        return f"{len(self.cards)}"
+
+
+class Player:
+    def __init__(self, name, cards):
+        self.name = name
+        self.cards = cards
+
+    def show_deck(self) -> None:
+        """Displays all player's hand in console."""
+        for card in self.cards:
+            logger.info(card)
+
+    def bet(self) -> Card:
+        """Remove the top card from player's hand and return it"""
+        return self.cards.pop(0)
+
+    def add_cards(self, cards_pit: list) -> None:
+        """Add cards from pit to the bottom of the player's hand"""
+        for card in cards_pit:
+            self.cards.append(card)
+
+    def __str__(self) -> str:
+        return f"{self.name.capitalize()} has {self.cards} cards."
+
+
+if __name__ == "__main__":
     deck = Deck()
-    deck.showDeck()
+    player1 = Player("player1", deck.cards[: int(len(deck.cards) / 2)])
+    player1.show_deck()
