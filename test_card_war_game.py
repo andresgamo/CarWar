7f1dbeccae7dab08b117d3e0c0babdd83cc5b2ca
andresgamo <andresgarciamolina3@gmail.com>
2023-09-28 18:22:07 -0500

    Added test for Deck Class

--- test_card_war_game.py
+++ test_card_war_game.py
@@ -9,7 +9,6 @@ class testCard(unittest.TestCase):
 
     def test_card_init(self):
         """Test initialization attributes"""
-        self.assertIsNotNone(self.card)
         self.assertEqual(self.card.rank, "A")
         self.assertEqual(self.card.suit, "♠")
         self.assertEqual(self.card.value, 14)
@@ -17,6 +16,36 @@ class testCard(unittest.TestCase):
     def test_card_str(self):
         """Test __str__ method"""
         self.assertEqual(str(self.card), "A♠")
+
+
+class testDeck(unittest.TestCase):
+    def setUp(self):
+        """Instansiate test case"""
+        self.deck = Deck()
+
+    def test_deck_init(self):
+        """Test initialization attributes"""
+        self.assertEqual(len(self.deck.cards), 52)
+
+    def test_deck_str(self):
+        """Test __str__ method"""
+        self.assertEqual(str(self.deck), "52")
+
+    def test_deck_len(self):
+        """Test __len__ method"""
+        self.assertEqual(len(self.deck), 52)
+
+    def test_shuffle(self):
+        """Test shuffle method"""
+        original = self.deck.cards.copy()
+        self.deck.shuffle()
+        self.assertNotEqual(original, self.deck.cards)
+
+    def test_split(self):
+        """Test split method"""
+        hands = self.deck.split(3)
+        self.assertEqual(len(hands), 3)
+        self.assertTrue(all(len(hand) == 17 for hand in hands))
 
 
 if __name__ == "__main__":
