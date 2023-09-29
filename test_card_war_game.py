160aeda4e270da91dc3bde1c7333d4cc4e9fb2fd
andresgamo <andresgarciamolina3@gmail.com>
2023-09-29 11:15:11 -0500

    Added test for Player Class

--- test_card_war_game.py
+++ test_card_war_game.py
@@ -26,6 +26,8 @@ class testDeck(unittest.TestCase):
     def test_deck_init(self):
         """Test initialization attributes"""
         self.assertEqual(len(self.deck.cards), 52)
+        self.assertEqual(str(self.deck.cards[0]), "2♣")
+        self.assertEqual(str(self.deck.cards[-1]), "A♠")
 
     def test_deck_str(self):
         """Test __str__ method"""
@@ -46,6 +48,37 @@ class testDeck(unittest.TestCase):
         hands = self.deck.split(3)
         self.assertEqual(len(hands), 3)
         self.assertTrue(all(len(hand) == 17 for hand in hands))
+
+
+class testPlayer(unittest.TestCase):
+    def setUp(self):
+        self.hand = [Card("A", "♠"), Card("2", "♣"), Card("3", "♦"), Card("4", "♥")]
+        self.player = Player("user_test", self.hand)
+
+    def test_init(self):
+        """Test initialization attributes"""
+        self.assertEqual(self.player.name, "user_test")
+        self.assertEqual(self.player.cards, self.hand)
+
+    def test_len(self):
+        """Test __len__ method"""
+        self.assertEqual(len(self.player), 4)
+
+    def test_str(self):
+        """Test __str__ method"""
+        self.assertEqual(str(self.player), "User_test has 4 cards.")
+
+    def test_bet(self):
+        """Test bet method"""
+        top_card = self.hand[0]
+        self.assertEqual(self.player.bet(), top_card)
+
+    def test_add_cards(self):
+        """Test add_cards method"""
+        pit = [Card("5", "♠")]
+        self.player.add_cards(pit)
+        self.assertEqual(len(self.player), 5)
+        self.assertEqual(self.player.cards[-1], pit[0])
 
 
 if __name__ == "__main__":
